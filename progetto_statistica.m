197ae333c6b7321c6a248d28c4a2cd32019d202f
LisaGalizzi <95870236+LisaGalizzi@users.noreply.github.com>
2021-12-14 23:54:44 +0100

    Add files via upload

--- progetto_statistica.m
+++ progetto_statistica.m
@@ -3,15 +3,14 @@ T = readtable('DATA SET UFFICIALE.xlsx');
 
 %% RINOMINARE COLONNE
 % T.Month = datetime(T.Month,"Format","dd-MM-uuuu"); in realtà non serve 
-T.Properties.VariableNames = {'Rif_Mese','Emiss_C02_Carbo','Emiss_C02_GasNa','Emiss_C02_BenAe','Emiss_C02_CoODi','Emiss_C02_LiqId','Emiss_C02_CarJe','Emiss_C02_Keros','Emiss_C02_Lubri','Emiss_C02_BenMo','Emiss_C02_CokPe','Emiss_C02_CoORe','Emiss_C02_AltrP','Emiss_C02_Petro','Emiss_C02_NTotE','Produzi_Carbone','Produz_GasNatur','Produz_PetrGreg','Produzi_CFosTOT','Produz_Idroelet','Produzio_Eolica','Produz_Biomasse','Produz_RinnoTOT','Produz_EnePrTOT','Consum_RinnoTOT','Consum_NRinnTOT','Consum_EnePrTOT','Import_EnePrTOT','Import_PetrOPEC','Consum_CFosTras','CDD','HDD'}
+T.Properties.VariableNames = {'Rif_Mese','Emiss_C02_Carbo','Emiss_C02_GasNa','Emiss_C02_BenAe','Emiss_C02_CoODi','Emiss_C02_LiqId','Emiss_C02_CarJe','Emiss_C02_Keros','Emiss_C02_Lubri','Emiss_C02_BenMo','Emiss_C02_CokPe','Emiss_C02_CoORe','Emiss_C02_AltrP','Emiss_C02_Petro','Emiss_C02_NTotE','Produz_Carbone','Produz_GasNatur','Produz_PetrGreg','Produz_CFosTOT','Produz_Idroelet','Produz_Eolica','Produz_Biomasse','Produz_RinnoTOT','Produz_EnePrTOT','Consum_RinnoTOT','Consum_NRinnTOT','Consum_EnePrTOT','Import_EnePrTOT','Import_PetrOPEC','Consum_CFosTras','CDD','HDD'}
 
 %% PLOT DATA
-
 f1 = figure('Position',[100,100,1250,675])  %Scelta dimensioni
-plot(T.Rif_Mese,T.Emiss_C02_NTotE,'Linewidth',1.3)
-xlabel('Anni')
-ylabel('Emissioni CO_{2}')
-title('Emissioni CO_{2} da carbone dal 1973-2021')
+plot(T.Rif_Mese,T.Emiss_C02_NTotE,"LineWidth",1.3)
+xlabel('Tempo [Anni]')
+ylabel('Emissioni CO_{2} [mln di tonnellate]')
+title('Emissioni CO_{2} per categoria. Anni: 1973-2021')
 hold on
 plot(T.Rif_Mese,T.Emiss_C02_Carbo,'Linewidth',1.3)
 plot(T.Rif_Mese,T.Emiss_C02_GasNa,'Linewidth',1.3)
@@ -19,32 +18,32 @@ plot(T.Rif_Mese,T.Emiss_C02_Petro,'Linewidth',1.3)
 ylim([0 700])
 legend('Emissioni CO_{2} TOT','Emissioni CO_{2} da Carbone','Emissioni CO_{2} da Gas Naturale','Emissioni CO_{2} da Petrolio')
 grid minor
-saveas(f1,[pwd '\immagini\1.ConfrontoEmissioni.png'])
+saveas(f1,[pwd '\immagini\1.ConfrontoEmissioniCO2.png'])
 
 f2 = figure('Position',[100,100,1250,675])  %Scelta dimensioni
-plot(T.Rif_Mese,T.Produzi_CFosTOT,'Linewidth',1.3)
-xlabel('Anni')
-ylabel('Emissioni CO_{2}')
-title('Emissioni CO_{2} da carbone dal 1973-2021')
+plot(T.Rif_Mese,T.Produz_CFosTOT,'LineWidth',1.3)
+xlabel('Tempo [Anni]')
+ylabel('Produzione energia non rinnovabile [quadrilioni di BTU]')
+title('Produzione energia non rinnovabile per categoria. Anni: 1973-2021')
 hold on
-plot(T.Rif_Mese,T.Produzi_Carbone,'Linewidth',1.3)
+plot(T.Rif_Mese,T.Produz_Carbone,'Linewidth',1.3)
 plot(T.Rif_Mese,T.Produz_GasNatur,'Linewidth',1.3)
 plot(T.Rif_Mese,T.Produz_PetrGreg,'Linewidth',1.3)
 plot(T.Rif_Mese,T.Consum_NRinnTOT,'Linewidth',1.3)
-legend('Prod CO_{2} TOT','Prod CO_{2} da Carbone','Prod CO_{2} da Gas Naturale','Emissioni CO_{2} da Petrolio','Consumo_NonRinnova')
+legend('Prod. Combustibili Fossili (Non Rinnovabile) TOT','Prod. Carbone','Prod. Gas Naturale','Prod. Petrolio Greggio','Consumo Energia Non Rinnovabile TOT')
 grid minor
 saveas(f2,[pwd '\immagini\2.ConfrontoProduzioneNonRinnovabili.png'])
 
 f3 = figure('Position',[100,100,1250,675])  %Scelta dimensioni
 plot(T.Rif_Mese,T.Produz_RinnoTOT,'Linewidth',1.3)
-xlabel('Anni')
-ylabel('Emissioni CO_{2}')
-title('Emissioni CO_{2} da carbone dal 1973-2021')
+xlabel('Tempo [Anni]')
+ylabel('Produzione energia rinnovabile [quadrilioni di BTU]')
+title('Produzione energia rinnovabile per categoria. Anni: 1973-2021')
 hold on
 plot(T.Rif_Mese,T.Produz_Idroelet,'Linewidth',1.3)
-plot(T.Rif_Mese,T.Produzio_Eolica,'Linewidth',1.3)
+plot(T.Rif_Mese,T.Produz_Eolica,'Linewidth',1.3)
 plot(T.Rif_Mese,T.Produz_Biomasse,'Linewidth',1.3)
 plot(T.Rif_Mese,T.Consum_RinnoTOT,'Linewidth',1.3)
-legend('Prod_Rinnovabili','Prod_idr','Prod_eol','Prod_biom','Consumo_Rinnov')
+legend('Prod. Energia Rinnovabile TOT','Prod. Energia Idroelettrica','Prod. Energia Eolica','Prod. Energia da Biomassa','Consumo Energia Rinnovabile TOT')
 grid minor
 saveas(f3,[pwd '\immagini\3.ConfrontoProduzioneRinnovabili.png'])
